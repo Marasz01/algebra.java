4a5798c607bbc56dcbc148aa2a5535de371f8784
IvanMaras <marasivan2001@gmail.com>
2024-04-10 19:14:48 +0200

    add js2

---
+++ skripte/script2.js
@@ -0,0 +1,16 @@
+var firstName, lastName, price, discount, fullPrice;
+var a;
+
+firstName = "John";
+lastName = "Doe";
+
+price = 19.9;
+discount = 0.1;
+
+var x = 1;
+if(x == 1){
+    x = 2;
+    console.log("x=" + x);
+
+}
+console.log("a=" + a);
